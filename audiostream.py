52ae55b68a2fc7b423b4ff27295e012ff2d79879
Sealgram <liam.seagram@greenwwoodcollege.com>
2022-01-29 12:16:03 -0500

    idk what i am doin

---
+++ audiostream.py
@@ -0,0 +1,16 @@
+import pyaudio
+
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+p = pyaudio.PyAudio()
+ 
+# starts recording
+stream = p.open(
+   format=FORMAT,
+   channels=CHANNELS,
+   rate=RATE,
+   input=True,
+   frames_per_buffer=FRAMES_PER_BUFFER
+)
--- speech.py
+++ speech.py
@@ -1,11 +1,8 @@
-import requests
-endpoint = "https://api.assemblyai.com/v2/transcript"
-json = {
-    "audio_url": "https://bit.ly/3yxKEIY"
-}
-headers = {
-    "authorization": "29787656f34d4dddb78dd19e8b6ffbdf",
-    "content-type": "application/json"
-}
-response = requests.post(endpoint, json=json, headers=headers)
-print(response.json())
+import websockets
+import asyncio
+import base64
+import json
+from configure import auth_key
+
+
+
